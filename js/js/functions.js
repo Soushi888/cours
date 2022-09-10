6603da8de8a860fd89bd1768a27d41b02179a5d4
Sacha Pignot <sacha.pignot.partner@decathlon.com>
2022-09-10 06:23:29 -0400

    functions

---
+++ js/js/functions.js
@@ -0,0 +1,121 @@
+"use strict";
+// Functions
+
+// Function calls
+function sayHi() { // functions names are in camelCase
+	console.log('Hi !');
+}
+
+sayHi();
+
+// Function calls with argument
+function greetings(name) {
+	console.log(`Hello ${name} !`);
+}
+
+greetings("Sacha");
+
+// Function calls with optional arguments
+function greetings2(name = "human") {
+	console.log(`Hi ${name} !`);
+}
+
+greetings2();
+greetings2("Soushi");
+
+// Function calls with return
+function add(num1, num2) {
+	return num1 + num2;
+}
+
+console.log("1 + 2 = " + add(1, 2));
+
+// Alternative syntax
+const subtract = function (num1, num2) {
+	return num1 - num2;
+}
+console.log("3 - 2 = " + subtract(3, 2));
+
+// arrow function with multiple lines
+const multiply = (num1, num2) => {
+	return num1 * num2;
+}
+console.log("3 x 2 = " + multiply(3, 2));
+
+// arrow function with one line (implicit return)
+const divide = (num1, num2) => num1 / num2;
+console.log("4 / 2 = " + divide(4, 2));
+
+// arrow function with one argument
+const square = num => num * num;
+console.log("2² = " + square(2));
+
+// arrow function with no argument
+const sayGoodbye = () => console.log("Goodbye !");
+sayGoodbye();
+
+function countAndEnumeratePeople(people) {
+	console.log(`There are ${people.length} people in the party : ${people.join(", ")}.`);
+}
+
+const people = ["Sacha Pignot", "Soushi888", "Shizore", "Shizoulfe"];
+const people2 = ["Louis", "Mathilde", "Sacha", "Jean", "Rose", "Soushi"];
+
+countAndEnumeratePeople(people);
+countAndEnumeratePeople(people2);
+
+// IIFE (Immediately Invoked Function Expression)
+(function () {
+	console.log("IIFE");
+})();
+
+// IIFE with arguments
+(function (name) {
+	console.log(`Hello ${name} !`);
+})("Soushi");
+
+// callback function
+function myForEach(array, callback) {
+	for (let i = 0; i < array.length; i++) {
+		callback(array[i]);
+	}
+}
+const quintuple = num => console.log(num * 5);
+
+myForEach([1, 2, 3], quintuple);
+[1,2,3].forEach(quintuple);
+
+// closure (a function that remembers its lexical scope even when the function is executed outside that lexical scope)
+function outer() {
+	let counter = 0;
+	function incrementCounter() {
+		counter++;
+	}
+	incrementCounter();
+	console.log(counter);
+}
+outer();
+
+// closure that returns a function
+function outer2() {
+	let counter = 0;
+	return function () {
+		counter++;
+		console.log(counter);
+	}
+}
+const inner = outer2(); // inner is a function
+inner();
+
+// closure that returns a function with arguments
+function outer3() {
+	let counter = 0;
+	return function (increment = 1) {
+		counter += increment;
+		console.log(counter);
+	}
+}
+const inner2 = outer3();
+inner2();
+inner2(2);
+
