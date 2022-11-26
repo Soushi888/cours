37cd574c8e6a56ee09ce2ef2c17034999ebcce60
Sacha Pignot <sacha.pignot.partner@decathlon.com>
2022-11-26 02:00:53 -0500

    bank-account exercise

---
+++ js/exercises/bank-account.js
@@ -0,0 +1,107 @@
+"use strict";
+
+class BankAccount {
+	constructor(name, initialBalance = 0) {
+		if (!BankAccount.validateAmount(initialBalance)) return;
+
+		this.name = name;
+		this.balance = initialBalance;
+	}
+
+	getBalance() {
+		console.log(`${this.name} has a balance of ${this.balance}.`);
+	}
+
+	deposit(amount) {
+		if (!BankAccount.validateAmount(amount)) return;
+
+		this.balance += amount;
+		return this.balance;
+	}
+
+	withdraw(subtractAmount) {
+		if (!BankAccount.validateWithdraw(subtractAmount, this.balance,)) return;
+
+		this.balance -= subtractAmount;
+		return this.balance;
+	}
+
+	transfer(amount, account) {
+		if (!BankAccount.validateWithdraw(amount, this.balance)) return;
+		if (!this.validateBankAccount(account)) return;
+
+		this.withdraw(amount);
+		account.deposit(amount);
+		return this.balance;
+	}
+
+	static validateAmount(amount) {
+		if (amount < 0) {
+			console.error("amount cannot be negative!");
+			return false;
+		}
+		return true;
+	}
+
+	static validateWithdraw(amount, balance) {
+		if (!BankAccount.validateAmount(amount)) return false;
+		if (amount > balance) {
+			console.error("amount cannot be greater than balance!");
+			return false;
+		}
+
+		return true
+	}
+
+	validateBankAccount(account) {
+		if (!(account instanceof BankAccount)) {
+			console.error("account must be a BankAccount!");
+			return false;
+		}
+		if (this === account) {
+			console.error("cannot transfer to self!");
+			return false;
+		}
+
+		return true
+	}
+}
+
+
+// Tests :
+
+let person1 = new BankAccount("Sacha", 1000);
+person1.getBalance();
+person1.deposit(200);
+person1.getBalance();
+person1.withdraw(330);
+person1.getBalance();
+person1.deposit(400);
+person1.deposit(300);
+person1.getBalance();
+
+let person2 = new BankAccount("nick");
+person2.getBalance();
+person1.transfer(100, person2);
+person1.getBalance();
+person2.getBalance();
+
+person1.withdraw(1500);
+person1.getBalance();
+person1.deposit(-100);
+person1.getBalance();
+
+person1.transfer(100, person1);
+person1.getBalance();
+person1.transfer(100, "person2");
+person1.getBalance();
+person1.transfer(1500, person2);
+person1.getBalance();
+
+console.log("")
+person1.transfer(-500, person2);
+person1.getBalance();
+console.log("")
+
+let person3 = new BankAccount("nico", -100);
+console.log(person3);
--- js/exercises/bank-account.md
+++ js/exercises/bank-account.md
@@ -1,13 +1,22 @@
-    # Bank Account
+# Bank Account
 
 ## Instructions
 
 Create a class `BankAccount` that implements the following requirements:
  - A bank account can be created with a name and an optional initial balance.
    - If the initial balance is not provided, the balance should be 0.
+   - Log an error and abort if the initial balance is negative.
  - The bank account should have a `getBalance` method that log the account name and the current balance.
  - The bank account should have a `deposit` method that accepts a number and adds it to the balance.
    - The `deposit` method should return the new balance.
-   - The `deposit` method should throw an error if the number is negative.
+   - The `deposit` method should log an error if the number is negative.
  - The bank account should have a `withdraw` method that accepts a number and subtracts it from the balance. 
-   - If the withdrawal amount is greater than the balance, throw an error.
+   - If the withdrawal amount is greater than the balance or if it is negative, log an error.
+ - Optional : The bank account should have a `transfer` method that accepts a number and a bank account.
+   - The `transfer` method should subtract the amount from the current account and add it to the other account.
+   - The `transfer` method should return the new balance of the current account.
+   - The `transfer` method should abort and log an error if : 
+     - The number is negative 
+     - The number is greater than the current balance
+     - The other account is not a `BankAccount` instance
+     - The other account is the same as the current account
